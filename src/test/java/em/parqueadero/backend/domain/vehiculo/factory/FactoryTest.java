f5ea3a257991ca47b9c568471dc7c4ae03df8c47
Edwin Didier Méndez Rojas - Ceiba Software <edwin.mendez@ceiba.com.co>
2018-11-07 08:57:08 -0500

    spy

--- src/test/java/em/parqueadero/backend/domain/vehiculo/factory/FactoryTest.java
+++ src/test/java/em/parqueadero/backend/domain/vehiculo/factory/FactoryTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,7 +24,7 @@ public class FactoryTest {
 
 	@Before
 	public void setUp() {
-		factory = mock(Factory.class);
+		factory = Mockito.spy(Factory.class);
 	}
 	
 	@Test
@@ -31,7 +32,7 @@ public class FactoryTest {
 		
 		// arrange
 		VehiculoModel moto = new VehiculoTestDataBuilder().setTipoVehiculo(VehiculoConstant.MOTO).build();
-		when(factory.getService(moto)).thenReturn( new MotoServiceImpl() );
+		when( factory.getService(moto) ).thenReturn( new MotoServiceImpl() );
 		
 		// act
 		VehiculoService vehiculoService = factory.getService(moto);
